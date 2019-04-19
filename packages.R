446fb856e082130c7baaa09e348c13b5b2c6ba7f
John Mount <jmount@win-vector.com>
2019-04-19 07:32:44 -0700

    add to package list

--- packages.R
+++ packages.R
@@ -1,16 +1,17 @@
 
-# Answer "n" to:
+# Please have an up to date version of R (3.5.*, or newer)
+# Answer "no" to:
 # Do you want to install from sources the packages which need compilation?
 update.packages(ask = FALSE, checkBuilt = TRUE)
 
 pkgs <- c(
-    "DBI", "RSQLite", "WVPlots", "arules", "bitops", "caTools",
-    "cdata", "data.table", "dplyr", "ggplot2", "gridExtra",
-    "hexbin", "kernlab", "knitr", "lime", "mgcv", "plotly", "pwr",
-    "randomForest", "readr", "readxls", "rmarkdown", "rpart",
-    "rpart.plot",
-    "rquery", "sigr", "sqldf", "tidyr", "vtreat", "wrapr",
-    "xgboost", "zoo" )
-install.packages(pkgs)
+  'arules', 'bitops', 'caTools', 'cdata', 'data.table', 'DBI',
+  'dplyr', 'e1071', 'fpc', 'gdata', 'ggplot2', 'glmnet', 'glmnetUtils',
+  'gridExtra', 'hexbin', 'kernlab', 'knitr', 'lime',
+  'lubridate', 'magrittr', 'MASS', 'mgcv', 'pander', 'plotly', 'pwr',
+  'randomForest', 'readr', 'readxls', 'rmarkdown', 'rpart',
+  'rpart.plot', 'rquery', 'RSQLite', 'scales', 'sigr', 'sqldf', 'tidyr',
+  'vtreat', 'wrapr', 'WVPlots', 'xgboost', 'xts', 'zeallot', 'zoo')
+install.packages(pkgs, dependencies = c("Depends", "Imports", "LinkingTo"))
 
-   
+
